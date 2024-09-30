1a51c21a0495ae906093b88cbae209e4aa6d4de1
Artem <48178500+ElusAegis@users.noreply.github.com>
2024-09-30 15:48:57 +0100

    (feat: core) Added functionality to hide dangerous parts of the message

--- src/bin/claud_reply.rs
+++ src/bin/claud_reply.rs
@@ -1,15 +1,14 @@
 use http_body_util::BodyExt;
 use hyper::client::conn::http1::SendRequest;
 use hyper::header::{CONNECTION, CONTENT_TYPE, HOST};
-use hyper::{Method, StatusCode};
+use hyper::{HeaderMap, Method, StatusCode};
 use hyper_util::rt::TokioIo;
 use notary_client::{Accepted, NotarizationRequest, NotaryClient};
 use serde_json::json;
-use spansy::http::{Request, Response};
+use std::ops::Range;
 use std::{env, str};
-use tlsn_core::proof::{SessionProof, SubstringsProofBuilder};
-use tlsn_core::{commitment::CommitmentKind, proof::TlsProof};
-use tlsn_formats::http::NotarizedHttpSession;
+use tlsn_core::commitment::CommitmentId;
+use tlsn_core::{proof::TlsProof, NotarizedSession};
 use tlsn_prover::tls::state::Closed;
 use tlsn_prover::tls::{Prover, ProverConfig, ProverControl, ProverError};
 use tokio::io::AsyncWriteExt as _;
@@ -22,6 +21,15 @@ use tracing::log::info;
 const SERVER_DOMAIN: &str = "api.anthropic.com";
 const ROUTE: &str = "/v1/messages";
 const SETUP_PROMPT: &str = "Setup Prompt: YOU ARE GOING TO BE ACTING AS A HELPFUL ASSISTANT";
+const REQUEST_TOPICS_TO_CENSOR: [&str; 1] = ["x-api-key"];
+const RESPONSE_TOPICS_TO_CENSOR: [&str; 6] = [
+    "anthropic-ratelimit-requests-reset",
+    "anthropic-ratelimit-tokens-reset",
+    "request-id",
+    "x-cloud-trace-context",
+    "cf-ray",
+    "date",
+];
 
 // Setting of the notary server — make sure these are the same with the config in ../../../notary/server
 const NOTARY_HOST: &str = "0.0.0.0";
@@ -31,14 +39,14 @@ const NOTARY_PORT: u16 = 7047;
 async fn main() {
     let (api_key, prover_ctrl, prover_task, mut request_sender) = setup_connections().await;
 
-
     let mut messages = vec![];
 
     let mut request_index = 1;
 
+    let mut recv_private_data = vec![];
+    let mut sent_private_data = vec![];
 
     loop {
-        
         let mut user_message = String::new();
         if request_index == 1 {
             user_message = SETUP_PROMPT.to_string();
@@ -68,6 +76,13 @@ async fn main() {
         // Prepare the Request to send to the OpenAI API
         let request = generate_request(&mut messages, &api_key);
 
+        // Collect the sent private data
+        extract_private_data(
+            &mut sent_private_data,
+            request.headers(),
+            REQUEST_TOPICS_TO_CENSOR.as_slice(),
+        );
+
         debug!("Request {request_index}: {:?}", request);
 
         debug!("Sending request {request_index} to OpenAI API...");
@@ -80,8 +95,18 @@ async fn main() {
 
         if response.status() != StatusCode::OK {
             // TODO - do a graceful shutdown
-            panic!("Request {request_index} failed with status: {}", response.status());
+            panic!(
+                "Request {request_index} failed with status: {}",
+                response.status()
+            );
         }
+
+        // Collect the received private data
+        extract_private_data(
+            &mut recv_private_data,
+            response.headers(),
+            RESPONSE_TOPICS_TO_CENSOR.as_slice(),
+        );
 
         // Collect the body
         let payload = response.into_body().collect().await.unwrap().to_bytes();
@@ -90,12 +115,15 @@ async fn main() {
             serde_json::from_str::<serde_json::Value>(&String::from_utf8_lossy(&payload)).unwrap();
 
         // Pretty printing the response
-        debug!("Response {request_index}: {}", serde_json::to_string_pretty(&parsed).unwrap());
-
+        debug!(
+            "Response {request_index}: {}",
+            serde_json::to_string_pretty(&parsed).unwrap()
+        );
 
         debug!("Request {request_index} to OpenAI succeeded");
 
-        let received_assistant_message = json!({"role": "assistant", "content": parsed["content"][0]["text"]});
+        let received_assistant_message =
+            json!({"role": "assistant", "content": parsed["content"][0]["text"]});
         messages.push(received_assistant_message);
 
         request_index += 1;
@@ -111,7 +139,7 @@ async fn main() {
         .header("Accept-Encoding", "identity")
         .header(CONNECTION, "close") // This will instruct the server to close the connection
         .header(CONTENT_TYPE, "application/json")
-        .header("x-api-key", api_key)
+        .header("x-api-key", api_key.clone())
         .header("anthropic-version", "2023-06-01")
         .body(String::new())
         .unwrap();
@@ -121,46 +149,41 @@ async fn main() {
     // As this is the last request, we can defer decryption until the end.
     prover_ctrl.defer_decryption().await.unwrap();
 
-    let shutdown_response = request_sender.send_request(close_connection_request).await.unwrap();
+    let shutdown_response = request_sender
+        .send_request(close_connection_request)
+        .await
+        .unwrap();
 
     // Collect the body
-    let payload = shutdown_response.into_body().collect().await.unwrap().to_bytes();
+    let payload = shutdown_response
+        .into_body()
+        .collect()
+        .await
+        .unwrap()
+        .to_bytes();
 
     let parsed =
         serde_json::from_str::<serde_json::Value>(&String::from_utf8_lossy(&payload)).unwrap();
 
     // Pretty printing the response
     // TODO - do another shutdown request that doesn't return an error
-    debug!("Shutdown response (expeted error): {}", serde_json::to_string_pretty(&parsed).unwrap());
+    debug!(
+        "Shutdown response (expeted error): {}",
+        serde_json::to_string_pretty(&parsed).unwrap()
+    );
 
+    let (sent_commitment_ids, received_commitment_ids, notarized_session) =
+        notirise_session(prover_task, &mut recv_private_data, &mut sent_private_data).await;
 
-    // Notarize the session
-    let notarized_session = notarise_the_session(prover_task).await;
     debug!("Notarization complete!");
 
-
-    let session_proof : SessionProof = notarized_session.session_proof();
-
-    let mut proof_builder : SubstringsProofBuilder = notarized_session.session().data().build_substrings_proof();
-
-
-    // For each request unless the last one, reveal the request and response substrings
-    for i in 0..request_index - 1 {
-        let request = &notarized_session.transcript().requests[i];
-        let response = &notarized_session.transcript().responses[i];
-
-        reveal_request(&mut proof_builder, request);
-        reveal_response(&mut proof_builder, response);
-    }
-
-
     // Build the proof
-    let substrings_proof = proof_builder.build().unwrap();
 
-    let proof = TlsProof {
-        session: session_proof,
-        substrings: substrings_proof,
-    };
+    let proof = build_proof(
+        sent_commitment_ids,
+        received_commitment_ids,
+        notarized_session,
+    );
 
     // Dump the proof to a file.
     let mut file = tokio::fs::File::create("claud_response_proof.json")
@@ -171,65 +194,130 @@ async fn main() {
         .unwrap();
 }
 
-fn reveal_response(proof_builder: &mut SubstringsProofBuilder, response: &Response) {
-    proof_builder
-        .reveal_recv(response, CommitmentKind::Blake3)
-        .unwrap();
+fn build_proof(
+    sent_commitment_ids: Vec<CommitmentId>,
+    received_commitment_ids: Vec<CommitmentId>,
+    notarized_session: NotarizedSession,
+) -> TlsProof {
+    let session_proof = notarized_session.session_proof();
 
-    // let disclosed_headers = vec!["via", "content-length", "anthropic-version", "content-type"];
-    // let shown_headers = vec![];
-    // 
-    // for header in &response.headers {
-    //     if disclosed_headers.contains(&header.name.as_str().to_ascii_lowercase().as_str()) {
-    //         proof_builder
-    //             .reveal_sent(header, CommitmentKind::Blake3)
-    //             .unwrap();
-    //     } else if shown_headers.contains(&header.name.as_str().to_ascii_lowercase().as_str()) {
-    //         proof_builder
-    //             .reveal_sent(&header.without_value(), CommitmentKind::Blake3)
-    //             .unwrap();
-    //     }
-    // }
+    let mut proof_builder = notarized_session.data().build_substrings_proof();
+
+    for id in sent_commitment_ids {
+        proof_builder.reveal_by_id(id).unwrap();
+    }
+    for id in received_commitment_ids {
+        proof_builder.reveal_by_id(id).unwrap();
+    }
+
+    let substrings_proof = proof_builder.build().unwrap();
+
+    let proof = TlsProof {
+        session: session_proof,
+        substrings: substrings_proof,
+    };
+    proof
 }
 
-fn reveal_request(proof_builder: &mut SubstringsProofBuilder, request: &Request) {
-    proof_builder
-        .reveal_sent(request, CommitmentKind::Blake3)
-        .unwrap();
-
-    // let disclosed_headers = vec!["host", "content-length", "anthropic-version", "accept-encoding"];
-    // let shown_headers = vec![""];
-    //
-    // for header in &request.headers {
-    //     // Only reveal the Host and Authorization headers
-    //     if disclosed_headers.contains(&header.name.as_str().to_ascii_lowercase().as_str()) {
-    //         proof_builder
-    //             .reveal_sent(header, CommitmentKind::Blake3)
-    //             .unwrap();
-    //     } else if shown_headers.contains(&header.name.as_str().to_ascii_lowercase().as_str()) {
-    //         proof_builder
-    //             .reveal_sent(&header.without_value(), CommitmentKind::Blake3)
-    //             .unwrap();
-    //     }
-    // }
-}
-
-async fn notarise_the_session(prover_task: JoinHandle<Result<Prover<Closed>, ProverError>>) -> NotarizedHttpSession {
+async fn notirise_session(
+    prover_task: JoinHandle<Result<Prover<Closed>, ProverError>>,
+    recv_private_data: &mut Vec<Vec<u8>>,
+    sent_private_data: &mut Vec<Vec<u8>>,
+) -> (Vec<CommitmentId>, Vec<CommitmentId>, NotarizedSession) {
     // The Prover task should be done now, so we can grab it.
     let prover = prover_task.await.unwrap().unwrap();
 
-    // Upgrade the prover to an HTTP prover, and start notarization.
-    let mut prover = prover.to_http().unwrap().start_notarize();
+    // Prepare for notarization
+    let mut prover = prover.start_notarize();
 
-    // Commit to the transcript with the default committer, which will commit using BLAKE3.
-    prover.commit().unwrap();
+    // Notarize the session
+    let (public_sent_commitment_ids, _) = find_ranges(
+        prover.sent_transcript().data(),
+        &sent_private_data
+            .iter()
+            .map(|v| v.as_slice())
+            .collect::<Vec<&[u8]>>(),
+    );
 
-    // Finalize, returning the notarized HTTP session
+    let (public_received_commitment_ids, _) = find_ranges(
+        prover.recv_transcript().data(),
+        &recv_private_data
+            .iter()
+            .map(|v| v.as_slice())
+            .collect::<Vec<&[u8]>>(),
+    );
+
+    let builder = prover.commitment_builder();
+
+    let sent_commitment_ids = public_sent_commitment_ids
+        .iter()
+        .map(|range| builder.commit_sent(range).unwrap())
+        .collect::<Vec<_>>();
+
+    let recived_commitment_ids = public_received_commitment_ids
+        .iter()
+        .map(|range| builder.commit_recv(range).unwrap())
+        .collect::<Vec<_>>();
+
+    // Finalize, returning the notarized session
     let notarized_session = prover.finalize().await.unwrap();
-    notarized_session
+    (
+        sent_commitment_ids,
+        recived_commitment_ids,
+        notarized_session,
+    )
 }
 
-async fn setup_connections() -> (String, ProverControl, JoinHandle<Result<Prover<Closed>, ProverError>>, SendRequest<String>) {
+fn extract_private_data(
+    recv_private_data: &mut Vec<Vec<u8>>,
+    headers: &HeaderMap,
+    topics_to_censor: &[&str],
+) {
+    for (header_name, header_value) in headers {
+        if topics_to_censor.contains(&header_name.as_str()) {
+            let header_value = header_value.as_bytes().to_vec();
+            if !recv_private_data.contains(&header_value) {
+                recv_private_data.push(header_value);
+            }
+        }
+    }
+}
+
+fn find_ranges(seq: &[u8], sub_seq: &[&[u8]]) -> (Vec<Range<usize>>, Vec<Range<usize>>) {
+    let mut private_ranges = Vec::new();
+    for s in sub_seq {
+        for (idx, w) in seq.windows(s.len()).enumerate() {
+            if w == *s {
+                private_ranges.push(idx..(idx + w.len()));
+            }
+        }
+    }
+
+    let mut sorted_ranges = private_ranges.clone();
+    sorted_ranges.sort_by_key(|r| r.start);
+
+    let mut public_ranges = Vec::new();
+    let mut last_end = 0;
+    for r in sorted_ranges {
+        if r.start > last_end {
+            public_ranges.push(last_end..r.start);
+        }
+        last_end = r.end;
+    }
+
+    if last_end < seq.len() {
+        public_ranges.push(last_end..seq.len());
+    }
+
+    (public_ranges, private_ranges)
+}
+
+async fn setup_connections() -> (
+    String,
+    ProverControl,
+    JoinHandle<Result<Prover<Closed>, ProverError>>,
+    SendRequest<String>,
+) {
     tracing_subscriber::fmt::init();
 
     // Load secret variables from environment for OpenAI API connection
@@ -295,14 +383,16 @@ async fn setup_connections() -> (String, ProverControl, JoinHandle<Result<Prover
     (api_key, prover_ctrl, prover_task, request_sender)
 }
 
-fn generate_request(messages: &mut Vec<serde_json::Value>, api_key: &str) -> hyper::Request<String> {
+fn generate_request(
+    messages: &mut Vec<serde_json::Value>,
+    api_key: &str,
+) -> hyper::Request<String> {
     let messages = serde_json::to_value(messages).unwrap();
     let mut json_body = serde_json::Map::new();
     json_body.insert("model".to_string(), json!("claude-3-5-sonnet-20240620"));
     json_body.insert("max_tokens".to_string(), json!(1024));
     json_body.insert("messages".to_string(), messages);
     let json_body = serde_json::Value::Object(json_body);
-
 
     // Build the HTTP request to send the prompt to OpenAI API
     hyper::Request::builder()
