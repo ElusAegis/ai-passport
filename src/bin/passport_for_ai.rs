29945c1f4a4aad1ebfcc64d64d1d057f57894d49
Artem <48178500+ElusAegis@users.noreply.github.com>
2025-08-15 16:24:24 +0300

    (chore) remove default logging

--- src/bin/passport_for_ai.rs
+++ src/bin/passport_for_ai.rs
@@ -1,9 +1,18 @@
 use anyhow::Result;
 use passport_for_ai::Application;
+use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            EnvFilter::from_default_env()
+                // Only show WARN+ from all crates by default…
+                .add_directive("warn".parse()?)
+                // …but still allow INFO for your crate
+                .add_directive("passport_for_ai=info".parse()?),
+        )
+        .init();
 
     print_welcome_message();
 
